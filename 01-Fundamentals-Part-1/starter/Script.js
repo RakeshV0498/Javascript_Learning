81e8c4d2d806fc599ebc5be23b67a68ea59ab2a2
RakeshV0498 <rockr4674@gmail.com>
2024-01-04 02:48:04 +0530

    Upated Code

---
+++ 01-Fundamentals-Part-1/starter/Script.js
@@ -0,0 +1,13 @@
+let js = "amazing";
+console.log(40 + 8 + 5 + 6);
+
+let firstName = "Jane";
+console.log(firstName);
+
+let country = "India";
+let continent = "Asia";
+let population = "140 Million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
