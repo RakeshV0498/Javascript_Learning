67c27fd24c3a253989ff6712ea608a9c9937cc34
RakeshV0498 <rockr4674@gmail.com>
2024-01-10 20:43:43 +0530

    Updated Code

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,42 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+//to select mulitple elements with same class name we should use queryselectorall
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// console.log(btnsOpenModal, typeof btnsOpenModal);
+
+//we can also use for of loop for objects
+// for (const i of btnsOpenModal) {
+//   console.log(i.textContent);
+// }
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  //   console.log(`${btnsOpenModal[i].textContent} is clicked`);
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//   btnsOpenModal[i].addEventListener('click', openModal);
+// }
+
+for (const i of btnsOpenModal) {
+  i.addEventListener('click', openModal);
+}
+//closemodal function only will be called when the click event is happened
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (eventKey) {
+  if (eventKey.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
