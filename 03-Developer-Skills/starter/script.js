e2b90a3afb0eb8c1c2a27b7d43380ee2943d0573
RakeshV0498 <rockr4674@gmail.com>
2024-01-10 11:39:07 +0530

    updated codes

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const birthYear = 1998;
+
+const calcAge = (birthYear) => 2024 - birthYear;
+
+console.log(calcAge(birthYear));
+
+console.log("hello there");
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`Your number is ${i}`);
+  console.log("Hi there");
+}
