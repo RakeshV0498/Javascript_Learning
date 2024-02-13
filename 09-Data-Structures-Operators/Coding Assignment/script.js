23256eeb9d3bff4811291dc7f487230caefcecba
RakeshV0498 <rockr4674@gmail.com>
2024-02-13 19:10:48 +0530

    Second Commit

--- 09-Data-Structures-Operators/Coding Assignment/script.js
+++ 09-Data-Structures-Operators/Coding Assignment/script.js
@@ -1,25 +1,25 @@
 const books = [
   {
-    title: "Algorithms",
-    author: ["Robert Sedgewick", "Kevin Wayne"],
-    publisher: "Addison-Wesley Professional",
-    publicationDate: "2011-03-24",
+    title: 'Algorithms',
+    author: ['Robert Sedgewick', 'Kevin Wayne'],
+    publisher: 'Addison-Wesley Professional',
+    publicationDate: '2011-03-24',
     edition: 4,
     keywords: [
-      "computer science",
-      "programming",
-      "algorithms",
-      "data structures",
-      "java",
-      "math",
-      "software",
-      "engineering",
+      'computer science',
+      'programming',
+      'algorithms',
+      'data structures',
+      'java',
+      'math',
+      'software',
+      'engineering',
     ],
     pages: 976,
-    format: "hardcover",
-    ISBN: "9780321573513",
-    language: "English",
-    programmingLanguage: "Java",
+    format: 'hardcover',
+    ISBN: '9780321573513',
+    language: 'English',
+    programmingLanguage: 'Java',
     onlineContent: true,
     thirdParty: {
       goodreads: {
@@ -33,27 +33,27 @@ const books = [
     highlighted: true,
   },
   {
-    title: "Structure and Interpretation of Computer Programs",
+    title: 'Structure and Interpretation of Computer Programs',
     author: [
-      "Harold Abelson",
-      "Gerald Jay Sussman",
-      "Julie Sussman (Contributor)",
+      'Harold Abelson',
+      'Gerald Jay Sussman',
+      'Julie Sussman (Contributor)',
     ],
-    publisher: "The MIT Press",
-    publicationDate: "2022-04-12",
+    publisher: 'The MIT Press',
+    publicationDate: '2022-04-12',
     edition: 2,
     keywords: [
-      "computer science",
-      "programming",
-      "javascript",
-      "software",
-      "engineering",
+      'computer science',
+      'programming',
+      'javascript',
+      'software',
+      'engineering',
     ],
     pages: 640,
-    format: "paperback",
-    ISBN: "9780262543231",
-    language: "English",
-    programmingLanguage: "JavaScript",
+    format: 'paperback',
+    ISBN: '9780262543231',
+    language: 'English',
+    programmingLanguage: 'JavaScript',
     onlineContent: false,
     thirdParty: {
       goodreads: {
@@ -68,23 +68,23 @@ const books = [
   },
   {
     title: "Computer Systems: A Programmer's Perspective",
-    author: ["Randal E. Bryant", "David Richard O'Hallaron"],
-    publisher: "Prentice Hall",
-    publicationDate: "2002-01-01",
+    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
+    publisher: 'Prentice Hall',
+    publicationDate: '2002-01-01',
     edition: 1,
     keywords: [
-      "computer science",
-      "computer systems",
-      "programming",
-      "software",
-      "C",
-      "engineering",
+      'computer science',
+      'computer systems',
+      'programming',
+      'software',
+      'C',
+      'engineering',
     ],
     pages: 978,
-    format: "hardcover",
-    ISBN: "9780130340740",
-    language: "English",
-    programmingLanguage: "C",
+    format: 'hardcover',
+    ISBN: '9780130340740',
+    language: 'English',
+    programmingLanguage: 'C',
     onlineContent: false,
     thirdParty: {
       goodreads: {
@@ -98,25 +98,25 @@ const books = [
     highlighted: true,
   },
   {
-    title: "Operating System Concepts",
-    author: ["Abraham Silberschatz", "Peter B. Galvin", "Greg Gagne"],
-    publisher: "John Wiley & Sons",
-    publicationDate: "2004-12-14",
+    title: 'Operating System Concepts',
+    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+    publisher: 'John Wiley & Sons',
+    publicationDate: '2004-12-14',
     edition: 10,
     keywords: [
-      "computer science",
-      "operating systems",
-      "programming",
-      "software",
-      "C",
-      "Java",
-      "engineering",
+      'computer science',
+      'operating systems',
+      'programming',
+      'software',
+      'C',
+      'Java',
+      'engineering',
     ],
     pages: 921,
-    format: "hardcover",
-    ISBN: "9780471694663",
-    language: "English",
-    programmingLanguage: "C, Java",
+    format: 'hardcover',
+    ISBN: '9780471694663',
+    language: 'English',
+    programmingLanguage: 'C, Java',
     onlineContent: false,
     thirdParty: {
       goodreads: {
@@ -129,16 +129,16 @@ const books = [
     },
   },
   {
-    title: "Engineering Mathematics",
-    author: ["K.A. Stroud", "Dexter J. Booth"],
-    publisher: "Palgrave",
-    publicationDate: "2007-01-01",
+    title: 'Engineering Mathematics',
+    author: ['K.A. Stroud', 'Dexter J. Booth'],
+    publisher: 'Palgrave',
+    publicationDate: '2007-01-01',
     edition: 14,
-    keywords: ["mathematics", "engineering"],
+    keywords: ['mathematics', 'engineering'],
     pages: 1288,
-    format: "paperback",
-    ISBN: "9781403942463",
-    language: "English",
+    format: 'paperback',
+    ISBN: '9781403942463',
+    language: 'English',
     programmingLanguage: null,
     onlineContent: true,
     thirdParty: {
@@ -153,15 +153,15 @@ const books = [
     highlighted: true,
   },
   {
-    title: "The Personal MBA: Master the Art of Business",
-    author: "Josh Kaufman",
-    publisher: "Portfolio",
-    publicationDate: "2010-12-30",
-    keywords: ["business"],
+    title: 'The Personal MBA: Master the Art of Business',
+    author: 'Josh Kaufman',
+    publisher: 'Portfolio',
+    publicationDate: '2010-12-30',
+    keywords: ['business'],
     pages: 416,
-    format: "hardcover",
-    ISBN: "9781591843528",
-    language: "English",
+    format: 'hardcover',
+    ISBN: '9781591843528',
+    language: 'English',
     thirdParty: {
       goodreads: {
         rating: 4.11,
@@ -173,22 +173,22 @@ const books = [
     },
   },
   {
-    title: "Crafting Interpreters",
-    author: "Robert Nystrom",
-    publisher: "Genever Benning",
-    publicationDate: "2021-07-28",
+    title: 'Crafting Interpreters',
+    author: 'Robert Nystrom',
+    publisher: 'Genever Benning',
+    publicationDate: '2021-07-28',
     keywords: [
-      "computer science",
-      "compilers",
-      "engineering",
-      "interpreters",
-      "software",
-      "engineering",
+      'computer science',
+      'compilers',
+      'engineering',
+      'interpreters',
+      'software',
+      'engineering',
     ],
     pages: 865,
-    format: "paperback",
-    ISBN: "9780990582939",
-    language: "English",
+    format: 'paperback',
+    ISBN: '9780990582939',
+    language: 'English',
     thirdParty: {
       goodreads: {
         rating: 4.7,
@@ -200,16 +200,16 @@ const books = [
     },
   },
   {
-    title: "Deep Work: Rules for Focused Success in a Distracted World",
-    author: "Cal Newport",
-    publisher: "Grand Central Publishing",
-    publicationDate: "2016-01-05",
+    title: 'Deep Work: Rules for Focused Success in a Distracted World',
+    author: 'Cal Newport',
+    publisher: 'Grand Central Publishing',
+    publicationDate: '2016-01-05',
     edition: 1,
-    keywords: ["work", "focus", "personal development", "business"],
+    keywords: ['work', 'focus', 'personal development', 'business'],
     pages: 296,
-    format: "hardcover",
-    ISBN: "9781455586691",
-    language: "English",
+    format: 'hardcover',
+    ISBN: '9781455586691',
+    language: 'English',
     thirdParty: {
       goodreads: {
         rating: 4.19,
@@ -222,3 +222,131 @@ const books = [
     highlighted: true,
   },
 ];
+
+// Task 1
+console.log(`------------------Task 1 --------------`);
+const [firstBook, secondBook] = books;
+
+console.log(firstBook, secondBook);
+
+// task 2
+console.log(`------------------Task 2 --------------`);
+
+const [, , thirdBook] = books;
+
+console.log(thirdBook);
+
+console.log(`------------------Task 3 --------------`);
+
+const ratings = [
+  ['rating', 4.19],
+  ['ratingsCount', 144584],
+];
+
+const [[, rating], [, ratingsCount]] = ratings;
+
+console.log(rating, ratingsCount);
+
+console.log(`------------------Task 4 --------------`);
+
+const ratingStars = [63405, 1808];
+
+const [fiveRatings, oneRatings, threeRatings = 0] = ratingStars;
+
+console.log(fiveRatings, oneRatings, threeRatings);
+
+console.log(`------------------Task 5 --------------`);
+
+const { title, author, ISBN } = books[0];
+
+console.log(title, author, ISBN);
+
+console.log(`------------------Task 6 --------------`);
+
+const { keywords: tags } = books[0];
+
+console.log(tags);
+
+console.log(`------------------Task 7 --------------`);
+
+const { language, programmingLanguage = 'unknown' } = books[6];
+
+console.log(language, programmingLanguage);
+
+console.log(`------------------Task 8 --------------`);
+
+let bookTitle = 'unknown';
+let bookAuthor = 'unknown';
+
+({ title: bookTitle, author: bookAuthor } = books[0]);
+
+console.log(bookTitle, bookAuthor);
+
+console.log(`------------------Task 9 --------------`);
+
+const {
+  thirdParty: {
+    goodreads: { rating: bookRating },
+  },
+} = books[0];
+
+console.log(bookRating);
+
+console.log(`------------------Task 10 --------------`);
+
+const printBookInfo = ({ title, author, year = 'Unknown' }) => {
+  console.log(`${title} by ${author},  ${year}`);
+};
+
+printBookInfo({
+  title: books[0].title,
+  author: books[0].author,
+  year: books[0].year,
+});
+
+console.log(`------------------Task 11 --------------`);
+
+const bookAuthors = [...books[0].author, ...books[1].author];
+
+console.log(bookAuthors);
+
+function spellWord(str) {
+  console.log(...str);
+}
+
+spellWord('hello');
+
+console.log(`------------------Task 12 --------------`);
+
+const [mainKeyword, ...rest] = books[0].keywords;
+
+console.log(mainKeyword, rest);
+
+console.log(`------------------Task 13 --------------`);
+
+const { publisher: bookPublisher, ...restOfTheBook } = books[1];
+
+console.log(bookPublisher, restOfTheBook);
+
+console.log(`------------------Task 14 --------------`);
+
+function printBookAuthorsCount(title, ...authors) {
+  console.log(`The book "${title}" has ${authors.length} authors`);
+}
+
+printBookAuthorsCount('Algorithms', 'Rakesh', 'Jonas');
+
+console.log(`------------------Task 15 --------------`);
+
+function hasExamplesInJava(book) {
+  return book.programmingLanguage === 'Java' || 'no data available';
+}
+
+console.log(hasExamplesInJava(books[1]));
+
+console.log(`------------------Task 16 --------------`);
+
+books.map(book => {
+  book.onlineContent &&
+    console.log(book.onlineContent && `${book.title} has online content`);
+});
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -28,6 +28,24 @@ const restaurant = {
       close: 24,
     },
   },
+  orderDelivery: function ({
+    startIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[startIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+  orderPizza: function (mainIng, ...otherIng) {
+    console.log(mainIng, otherIng);
+  },
 };
 
 const arr = [1, 2, 3, 4];
@@ -89,4 +107,276 @@ const [p = 1, q = 1, r = 1] = [8, , 45];
 
 console.log(p, q, r);
 
-//
+// Destructing Objects
+
+console.log(`--------------------------- Destructuring Objects--------------`);
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+console.log(`---------------Renaming Property name to variable name---------`);
+
+const {
+  name: restruantName,
+  categories: tags,
+  openingHours: hours,
+  // If we want different variable name from the property name, we need assign the property name to variable name like below
+  location: address,
+} = restaurant;
+
+console.log(restruantName);
+console.log(address);
+console.log(categories);
+console.log(openingHours);
+
+// Setting default values - to set the default value since we usually get the data from the outside source
+
+const { mainMenu: menu = [], starterMenu: starters = [] } = restaurant;
+
+console.log(menu);
+console.log(starters);
+
+// Mutating  variables
+
+let x = 111;
+let y = 999;
+
+const obj = { x: 23, y: 7, z: 14 };
+
+({ x, y } = obj);
+
+console.log(x, y);
+
+// Nested object destructing
+
+const {
+  fri: { open: O, close: C },
+} = restaurant.openingHours;
+
+console.log(O, C);
+
+//Object destructing functions
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Chennai, 21',
+  mainIndex: 2,
+  startIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Chennai-24',
+});
+
+// Spread Operators
+
+const testArr = [7, 8, 9];
+
+const newTestArr = [1, 2, testArr[0], testArr[1], testArr[2]];
+
+console.log(newTestArr);
+
+const newArr = [1, 2, ...testArr];
+
+console.log(newArr);
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnoccci'];
+
+console.log(newMenu);
+
+// Copy Arrray
+
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//Join 2 arrays or more
+
+const menus = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+console.log(menus);
+
+//Spead operator can be used on all iterables expect objects
+// Arrays,strings,maps,sets
+
+const spreadString = [...restaurant.name].join('-');
+
+console.log(spreadString);
+
+//Spread operator use with function
+
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3?'),
+// ];
+
+// restaurant.orderPasta(...ingredients);
+
+// Objects
+
+const newRestaurant = { ...restaurant, founder: 'Guiseppe', foundedIn: 1998 };
+
+newRestaurant.name = 'Ristronate Roma';
+
+console.log(newRestaurant);
+
+// Rest Pattern and Parameters
+
+// 1. Destructing
+
+// Spread operator because we use it right side of the (=) Operator
+const arrNew = [1, 2, ...[3, 4, 5]];
+
+// Rest Opeator because we use it left side of the (=) Operator
+const [i, j, ...others] = [1, 2, 3, 4, 5];
+
+console.log(i, j, others);
+
+// Combining both arrays using spread and rest
+
+const [pizza, , risotto, ...otherFoods] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+// const [pizza, ...otherFoods] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+
+// Rest operator will just collect the rest of the items, it will capture the in between elements, it will collect the elements after the last declared element
+
+// In the below output, it left out the pasta, since its in middle of both pizza and rosotto and the rest only takes after the last declared variable.
+
+// Rest operator should always be the last element
+
+// It doesnt include the skipped elements
+
+// Rest Operator should be used only once in the destructing assignments
+
+console.log(pizza, risotto, otherFoods);
+
+// Objects with Rest Operator
+
+const { sat, ...weekdays } = restaurant.openingHours;
+
+console.log(sat, weekdays);
+
+// 2. Functions
+
+const add = function (...numbers) {
+  const result = numbers.reduce((value, sum) => sum + value, 0);
+  console.log(result);
+};
+
+add(5, 4, 12);
+add(10, 15, 12, 13);
+add(...newArr);
+
+const multiply = function (...numbers) {
+  const result = numbers.reduce((value, acc) => value * acc, 1);
+  console.log(result);
+};
+
+multiply(1, 4, 3);
+multiply(...newArr);
+
+restaurant.orderPizza('mushrooms', 'onion', 'cheeze', 'spinach');
+
+// Short Circuiting(&& and ||)
+
+// If the first operand is a truthy value, then in case of OR operator it will always return the first operand. It wont even evaluate the second operand
+
+console.log(`------------------------or---------------`);
+
+console.log(3 || 'a');
+
+console.log('' || 'rakesh');
+
+console.log(true || 0);
+
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'hello' || 23 || null);
+
+// restaurant.numGuests = 23;
+
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+
+// console.log(guests1);
+
+// const guests2 = restaurant.numGuests || 10;
+
+// console.log(guests2);
+
+console.log(`------------------------AND---------------`);
+
+// If the first operand value is falsey, it returns the first operand value and not even evaluate the second operand value
+
+console.log(0 && 'Rakesh');
+
+// if the first operand value is truthy, it will evaluate the second operand value and if that also truthy value it will return the second operand vale.
+
+console.log(7 && 'Rakesh');
+
+// If there is a series of evaluation is going to happen, if any oeprand value is falsey value, then it will return that value and stops the flow.
+
+console.log(23 && 'hello' && null && undefined);
+
+// Practical Example
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('Mushroom', 'Cheese', 'Olives');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('Chicken', 'olives', 'cheese');
+
+console.log(`------------------Nullish Operator(??)--------------`);
+
+restaurant.numGuests = 0;
+
+const guests = restaurant.numGuests || 10;
+
+console.log(guests);
+
+// ?? operator works on Nullish Value null and undefined values (Not inclues 0 or "")
+
+const guestsCorrect = restaurant.numGuests ?? 10;
+
+console.log(guestsCorrect);
+
+// Three new logical assignement operators
+
+console.log(
+  `---------------Three New Logical Assignment Operators------------`
+);
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossai',
+};
+
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// Or Assignment Operator
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// To resolve the 0 zero issue
+// Nullish Assignment Operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// rest1.owner = rest1.owner && 'Annonymous';
+// rest2.owner = rest2.owner && 'Annonymous';
+
+rest1.owner &&= 'Annonymous';
+rest2.owner &&= 'Annonymous';
+
+console.log(rest1, rest2);
